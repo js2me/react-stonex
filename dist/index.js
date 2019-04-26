6489edd9c8876b51c3114d1b161f851963db4252
Volkov Sergey <truefunrun@hotmail.com>
2019-04-26 11:28:13 +0300

    feat: create connect function

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,24 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var Provider_1 = require("./components/Provider");
 exports.Provider = Provider_1.default;
 var SubscribeChanges_1 = require("./SubscribeChanges");
 exports.SubscribeChanges = SubscribeChanges_1.default;
-// function match (arg, factories, name) {
+// function match (arg: any, factories: any, name: any) {
 //   for (let i = factories.length - 1; i >= 0; i--) {
 //     const result = factories[i](arg)
 //     if (result) return result
 //   }
-//   return (dispatch, options) => {
+//   return (dispatch: any, options: any) => {
 //     throw new Error(
 //         `Invalid value of type ${typeof arg} for ${name} argument when connecting component ${
 //           options.wrappedComponentName
@@ -17,38 +26,37 @@ exports.SubscribeChanges = SubscribeChanges_1.default;
 //       )
 //   }
 // }
-// const strictEqual = (a1: any,a2: any) => a1 === a2
-// const shallowEqual = (objA: any, objB: any) => {
-//   if (strictEqual(objA, objB)) return true
-//   if (
-//     typeof objA !== 'object' ||
-//     objA === null ||
-//     typeof objB !== 'object' ||
-//     objB === null
-//   ) {
-//     return false
-//   }
-//   const keysA = Object.keys(objA)
-//   const keysB = Object.keys(objB)
-//   if (keysA.length !== keysB.length) return false
-//   for (const key of keysA) {
-//     if (!Object.prototype.hasOwnProperty.call(objB, key) || !strictEqual(objA[key], objB[key])) {
-//       return false
-//     }
-//   }
-//   return true
-// }
-// function connect (modulesToProps, mergeProps, {
-//       pure = true,
-//       areStatesEqual = strictEqual,
-//       areOwnPropsEqual = shallowEqual,
-//       areStatePropsEqual = shallowEqual,
-//       areMergedPropsEqual = shallowEqual,
-//       ...extraOptions
-//     } = {}): any {
-//   return (WrappedComponent) => {
-//   }
-// }
+var strictEqual = function (a1, a2) { return a1 === a2; };
+var shallowEqual = function (objA, objB) {
+    if (strictEqual(objA, objB))
+        return true;
+    if (typeof objA !== 'object' ||
+        objA === null ||
+        typeof objB !== 'object' ||
+        objB === null) {
+        return false;
+    }
+    var keysA = Object.keys(objA);
+    var keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length)
+        return false;
+    for (var _i = 0, keysA_1 = keysA; _i < keysA_1.length; _i++) {
+        var key = keysA_1[_i];
+        if (!Object.prototype.hasOwnProperty.call(objB, key) || !strictEqual(objA[key], objB[key])) {
+            return false;
+        }
+    }
+    return true;
+};
+function connect(modulesToProps, mergeProps, _a) {
+    if (_a === void 0) { _a = {}; }
+    var _b = _a.pure, pure = _b === void 0 ? true : _b, _c = _a.areStatesEqual, areStatesEqual = _c === void 0 ? strictEqual : _c, _d = _a.areOwnPropsEqual, areOwnPropsEqual = _d === void 0 ? shallowEqual : _d, _e = _a.areStatePropsEqual, areStatePropsEqual = _e === void 0 ? shallowEqual : _e, _f = _a.areMergedPropsEqual, areMergedPropsEqual = _f === void 0 ? shallowEqual : _f, extraOptions = __rest(_a, ["pure", "areStatesEqual", "areOwnPropsEqual", "areStatePropsEqual", "areMergedPropsEqual"]);
+    return function (WrappedComponent) {
+        console.log('WrappedComponent', WrappedComponent);
+        return WrappedComponent;
+    };
+}
+exports.connect = connect;
 // export { connect }
 /// connect(modulesToProps)(SomeComponent)
 // const modulesToProps = ({ books, items } = {}, ownProps = {}) => {
--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,13 @@
 import Provider from './components/Provider'
 import SubscribeChanges from './SubscribeChanges'
 
-export {
-    Provider,
-    SubscribeChanges,
-}
-
-// function match (arg, factories, name) {
+// function match (arg: any, factories: any, name: any) {
 //   for (let i = factories.length - 1; i >= 0; i--) {
 //     const result = factories[i](arg)
 //     if (result) return result
 //   }
 
-//   return (dispatch, options) => {
+//   return (dispatch: any, options: any) => {
 //     throw new Error(
 //         `Invalid value of type ${typeof arg} for ${name} argument when connecting component ${
 //           options.wrappedComponentName
@@ -21,47 +16,53 @@ export {
 //   }
 // }
 
-// const strictEqual = (a1: any,a2: any) => a1 === a2
-// const shallowEqual = (objA: any, objB: any) => {
-//   if (strictEqual(objA, objB)) return true
+const strictEqual = (a1: any,a2: any) => a1 === a2
+const shallowEqual = (objA: any, objB: any) => {
+  if (strictEqual(objA, objB)) return true
 
-//   if (
-//     typeof objA !== 'object' ||
-//     objA === null ||
-//     typeof objB !== 'object' ||
-//     objB === null
-//   ) {
-//     return false
-//   }
+  if (
+    typeof objA !== 'object' ||
+    objA === null ||
+    typeof objB !== 'object' ||
+    objB === null
+  ) {
+    return false
+  }
 
-//   const keysA = Object.keys(objA)
-//   const keysB = Object.keys(objB)
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
 
-//   if (keysA.length !== keysB.length) return false
+  if (keysA.length !== keysB.length) return false
 
-//   for (const key of keysA) {
-//     if (!Object.prototype.hasOwnProperty.call(objB, key) || !strictEqual(objA[key], objB[key])) {
-//       return false
-//     }
-//   }
+  for (const key of keysA) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key) || !strictEqual(objA[key], objB[key])) {
+      return false
+    }
+  }
 
-//   return true
-// }
+  return true
+}
 
-// function connect (modulesToProps, mergeProps, {
-//       pure = true,
-//       areStatesEqual = strictEqual,
-//       areOwnPropsEqual = shallowEqual,
-//       areStatePropsEqual = shallowEqual,
-//       areMergedPropsEqual = shallowEqual,
-//       ...extraOptions
-//     } = {}): any {
+function connect (modulesToProps: any, mergeProps: any, {
+      pure = true,
+      areStatesEqual = strictEqual,
+      areOwnPropsEqual = shallowEqual,
+      areStatePropsEqual = shallowEqual,
+      areMergedPropsEqual = shallowEqual,
+      ...extraOptions
+    } = {}): any {
 
-//   return (WrappedComponent) => {
+  return (WrappedComponent: any) => {
+    console.log('WrappedComponent', WrappedComponent)
+    return WrappedComponent
+  }
+}
 
-//   }
-// }
-
+export {
+    Provider,
+    SubscribeChanges,
+    connect,
+}
 // export { connect }
 
 /// connect(modulesToProps)(SomeComponent)
