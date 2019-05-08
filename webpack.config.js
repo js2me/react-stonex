fb5cd4ff46ee5b35b94aedca410f508a7ea83d81
Sergey Volkov <sergey.volkov@firstlinesoftware.com>
2019-05-08 15:52:59 +0300

    chore: rewrite library to JS solution (previously was TS)

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const resolve = require('path').resolve
+const path = dir => resolve(__dirname, dir)
+
+const createConfig = mode => {
+  const isProd = mode === 'production'
+  let filename = 'react-stonex'
+  return {
+    entry: path('lib/index.js'),
+    mode: mode,
+    output: {
+      path: path('dist'),
+      filename: (isProd ? `${filename}.min` : filename) + '.js',
+      library: filename,
+      libraryTarget: 'umd',
+    },
+    resolveLoader: {
+      modules: ['node_modules', path('loaders')],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loaders: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  }
+}
+
+module.exports = [createConfig('development')]
