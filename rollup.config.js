31bdc81ebefe360376ed185e2cef46793701fe63
Volkov Sergey <truefunrun@hotmail.com>
2019-05-12 17:19:39 +0300

    chore: reduce rollup config (removed useless dependencies)

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,13 @@
 const babel = require('rollup-plugin-babel')
-const resolve = require('rollup-plugin-node-resolve')
-const commonjs = require('rollup-plugin-commonjs')
+// const resolve = require('rollup-plugin-node-resolve')
+// const commonjs = require('rollup-plugin-commonjs')
 
-module.exports = {
-  input: 'lib/index.js',
-  output: { file: 'dist/react-stonex.js', format: 'cjs', indent: false },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    commonjs(),
-  ],
-}
+module.exports = [
+  // CJS
+  {
+    input: 'lib/index.js',
+    output: { file: 'dist/react-stonex.js', format: 'cjs', indent: false },
+    plugins: [babel()],
+    external: ['react'],
+  },
+]
