8a8b37bba3b2048f9290f19727763802f2b3b6cb
Volkov Sergey <truefunrun@hotmail.com>
2019-05-16 09:26:38 +0300

    feat: created a function comparer

--- src/Provider.js
+++ src/Provider.js
@@ -6,6 +6,51 @@ import {
   unsubscribeFromStateChanges,
 } from './SubscriberHelpers'
 
+const compareTwoValues = (nextValue, prevValue) => {
+  const nextType = typeof nextValue
+  const prevType = typeof prevValue
+
+  if (nextType !== prevType) {
+    return false
+  }
+
+  if (nextValue instanceof Array) {
+    if (!(prevValue instanceof Array)) {
+      return false
+    }
+
+    if (nextValue.length !== prevValue.length) {
+      return false
+    }
+
+    for (let x = 0; x < nextValue.length; x++) {
+      if (!compareTwoValues(nextValue[x], prevValue[x])) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  if (nextType === 'object') {
+    const keys = Object.keys(nextValue)
+
+    if (keys.length !== Object.keys(prevValue).length) {
+      return false
+    }
+
+    for (let x = 0; x < keys.length; x++) {
+      if (!compareTwoValues(nextValue[keys[x]], prevValue[keys[x]])) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  return nextValue === prevValue
+}
+
 export default class Provider extends React.Component {
   constructor(props) {
     super(props)
@@ -27,6 +72,8 @@ export default class Provider extends React.Component {
   componentWillUnmount() {
     unsubscribeFromStateChanges(this.props.store, this.whenStateChanged)
   }
+
+  shouldComponentUpdate() {}
 
   render() {
     const { children, store } = this.props
